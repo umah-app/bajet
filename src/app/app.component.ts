f6a64fdcadaa8eca95c79512a6aba303fbefbcc0
Teoh Han Hui <teohhanhui@gmail.com>
2024-05-21 04:58:10 +0800

    Add Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CurrencyPipe, MatTableModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  displayedColumns: string[] = ['name', 'amount'];
+  entries = computed(() => [{ name: 'Hello', amount: '100.00' }]);
   title = 'bajet';
 }
