09e4fde2d065123d50ea42a1eece0e1672738739
Teoh Han Hui <teohhanhui@gmail.com>
2024-05-24 02:03:10 +0800

    Add a FAB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component, computed, signal } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
+import { MatFabButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CurrencyPipe, MatTableModule, RouterOutlet],
+  imports: [CurrencyPipe, MatFabButton, MatIconModule, MatTableModule, MatTooltipModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
